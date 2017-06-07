088ea82388a7ab0035dabfda3ce0e27999ed1952
hardint5056 <hardint5056@student.faytechcc.edu>
2017-06-07 18:56:33 -0400

    Add files via upload
    Sales Prediction Program

---
+++ M2T1_hardin.py
@@ -0,0 +1,6 @@
+#Calculate the total sales times 23% from data input at the keyboard
+#6/7/2017
+#CTI-110 M2T1 - Sales Prediction
+total_sales = float(input('Enter the projected sales:'))
+profit = total_sales * 0.23
+print("The profit is $", format(profit, ',.2f'))
