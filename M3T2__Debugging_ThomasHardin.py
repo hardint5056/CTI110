731dab2c0f16b8ac5b670f000560013a277e2e82
hardint5056 <hardint5056@student.faytechcc.edu>
2017-06-20 17:50:15 -0400

    Add files via upload

---
+++ M3T2__Debugging_ThomasHardin.py
@@ -0,0 +1,31 @@
+#Debugging A program
+#6/16/17
+#CTI-110 M3T2_Debugging
+#Thomas Hardin
+#
+#Corrected code i.e. debugged code.
+print('This Program has several nested if-else statements. That need to be properly aligned.')
+#Need to set some varibles to test the Boolean.
+A_score = 90
+B_score = 80
+C_score = 70
+D_score = 60
+F_score = 50
+#Accept input from the keyboard
+score = int(input('Enter your test score.'))
+if score >= A_score:
+    print('Your grade is A.')
+else:
+    if score >= B_score:
+        print('Your grade is B.')
+    else:
+         if score >= C_score:
+            print('Your grade is C.')
+         else:
+             if score >= D_score:
+                 print('Your grade is D.')
+             else:
+                  if score >= F_score:
+                      print('Your grade is F.')
+	
+	
